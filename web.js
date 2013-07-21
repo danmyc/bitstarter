508fa60a2a6fc8adc7ae9538f502258543f0b499
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2013-07-21 07:13:04 +0000

    try again

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
+var buf = new Buffer(256);
+len = buf.write(fs.readFileSync('./index.html', 'utf-8'), 0);
+
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('World!Hye');
 });
 
 var port = process.env.PORT || 5000;
