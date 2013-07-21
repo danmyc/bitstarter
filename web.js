07556d706c3f47ae3b72dc23f64ca9cd955d183e
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2013-07-21 07:02:41 +0000

    try

--- web.js
+++ web.js
@@ -1,17 +1,12 @@
 var express = require('express');
-var buf = new Buffer(256);
-len = buf.write(fs.readFileSync('./index.html', 'utf-8'), 0);
-var app = express();
-app.use(express.logger());
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(/* String(buf.toString('utf-8', 0, len)) */ 'Hye');
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-// var buffer = new buffer(50);
-// buffer.toString(fs.readFileSync('./index.html'));
