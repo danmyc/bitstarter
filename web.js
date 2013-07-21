7e269e98ed912eea5227da88f98b43c05fb5c4b0
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2013-07-21 06:16:13 +0000

    Updated web.js again

--- web.js
+++ web.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-/* app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-}); */
+app.get('/', function(request, response) {
+  response.send(buffer.toString(fs.readFileSync('./index.html')));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
 
-buffer.toString(fs.readFileSync('/index.html'));
+// var buffer = new buffer(50);
+// buffer.toString(fs.readFileSync('./index.html'));
