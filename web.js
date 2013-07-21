8307d03c7b969c573d391366b2d8a12c9dad049a
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2013-07-21 06:59:46 +0000

    this

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(/* String(buf.toString('utf-8', 0, len)) */ "Hye");
+  response.send(/* String(buf.toString('utf-8', 0, len)) */ 'Hye');
 });
 
 var port = process.env.PORT || 5000;
